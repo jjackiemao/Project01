d9056c431f4253170d7b5fccf4fb0845a1fe6179
jjackiemao <jjackiemao@gmail.com>
2024-02-28 14:32:29 +0100

    Inlämning 2

--- src/MainClass.java
+++ src/MainClass.java
@@ -12,12 +12,12 @@ public class MainClass {
         scanner.nextLine(); // Consume newline
 
         if (option == 1) {
-            System.out.println("Enter the English text:");
+            System.out.println("Enter text:");
             String englishText = scanner.nextLine();
             String morseCode = translator.toMorse(englishText);
             System.out.println("Translation: " + morseCode);
         } else if (option == 2) {
-            System.out.println("Enter the Morse code (use '*' for dot and '-' for dash):");
+            System.out.println("Enter code (use '*' for dot and '-' for dash):");
             String morseCode = scanner.nextLine();
             String englishText = translator.toEnglish(morseCode);
             System.out.println("Translation: " + englishText);
